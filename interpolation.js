44b39e310b5f4a0322c3239781d9c043ab082df1
ceciliawambui <47850604+ceciliawambui@users.noreply.github.com>
2019-10-06 16:49:46 +0300

    Add files via upload

---
+++ interpolation.js
@@ -0,0 +1,3 @@
+let myName = 'Cecilia';
+let myCity = 'Nairobi';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
---
+++ methods.js
@@ -0,0 +1,3 @@
+
+console.log('Codecademy'.toUpperCase());
+console.log('    Remove whitespace   '.trim());
